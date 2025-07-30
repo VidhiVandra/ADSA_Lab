62a9a9955226681d4bf41faf7332c0c7b7fa58e5
Vidhi Vandra <vidhi.2403794202@vcet.edu.in>
2025-07-30 10:07:41 +0530

    Add files via upload

---
+++ hashFunctionExample.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class hashFunctionExample {
+    // Basic hash function using modulo
+    public static int computeHash(String input, int hashSize) {
+        int hash = 0;
+        for (int i = 0; i < input.length(); i++) {
+            hash = (hash * 31 + input.charAt(i)) % hashSize; // 31 is a common multiplier
+        }
+        return hash;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // Get user input
+        System.out.print("Enter a string to hash: ");
+        String input = scanner.nextLine();
+
+        System.out.print("Enter the hash size : ");
+        int hashSize = scanner.nextInt();
+
+        // Compute and print the hash
+        int hashValue = computeHash(input, hashSize);
+        System.out.println("Hash value: " + hashValue);
+    }
+}
